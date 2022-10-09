a6119239f8505559d282baf051f3f1a14aed5f2e
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-10-09 13:02:17 +0200

    Add blank spec & first suite of test

---
+++ simple-calculator/calculator.spec.js
@@ -0,0 +1,14 @@
+describe('calculator.js',() => {
+    it('should add number to total', ()=> {
+        // TODO: write expectaion for adding function
+    });
+    it('should subtract number from total', ()=> {
+        // TODO: write expectaion for subtracting function
+    });
+    it('should multiply total by number', ()=> {
+        // TODO: write expectaion for multiplication function
+    });
+    it('should divide total by number', ()=> {
+        // TODO: write expectaion for divison function
+    });
+});
