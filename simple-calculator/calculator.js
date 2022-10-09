7aa0e2e71fd5f22576e572aca6f83e299317bbfa
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-10-09 09:51:46 +0200

    Build test ui via Jasmine :tada:

---
+++ simple-calculator/calculator.js
@@ -0,0 +1,23 @@
+function Calculator() {
+    this.total = 0;
+};
+
+
+Calculator.prototype.add =  function(n) {
+    return this.total += n;
+}
+
+Calculator.prototype.subtract = function(n){
+    return this.total -= n;
+}
+
+Calculator.prototype.multiply = function(n){
+    return this.total *= n;
+}
+
+Calculator.prototype.divide = function(n){
+    if(n === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return this.total /= n;
+}
---
+++ simple-calculator/main.js
@@ -0,0 +1,37 @@
+function updateResult (result) {
+    const element = document.getElementById('result');
+    if(element) {
+        element.innerText = result;
+    }
+}
+
+function calculate(inputValue) {
+    const expression = /\+|\-|\*|\//;
+    const numbers = inputValue.split(expression);
+    const numberA = Number(numbers[0]);
+    const numberB = Number(numbers[1]);
+    const matchOperation = inputValue.match(expression);
+    if(matchOperation === null || Number.isNaN(numberA) || Number.isNaN(numberB)) {
+        updateResult('Invalid operator');
+        return;
+    }
+    const operator = matchOperation[0];
+    const calculator = new Calculator();
+    calculator.add(numberA);
+    let result;
+    console.log(operator)
+    if(operator === '+') {
+        
+        result = calculator.add(numberB);
+    }
+    else if(operator === '-') {
+        result = calculator.subtract(numberB);
+    }
+    else if(operator === '*') {
+        result = calculator.multiply(numberB);
+    }
+    else if(operator === '/') {
+        result = calculator.divide(numberB);
+    }
+    updateResult(result);
+}
