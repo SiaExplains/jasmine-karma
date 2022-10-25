30c6102df6450f0505b14e4a9849760e89e7d7d4
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-10-25 12:20:05 +0200

    Spying on prototypes

--- simple-calculator/main.spec.js
+++ simple-calculator/main.spec.js
@@ -19,12 +19,36 @@ describe('main.js', () => {
             expect(window.updateResult).toHaveBeenCalled();
             expect(window.updateResult).toHaveBeenCalledTimes(1);
         });
-        xit('calls add');
-        xit('calls subtract');
-        xit('calls multiply');
-        xit('calls divide');
-        xit('calls divide');
-        xit('validates operation');
+        it('calls add', () => {
+            const spy = spyOn(Calculator.prototype, 'add');
+            calculate('3+4');
+
+            expect(Calculator.prototype.add).toHaveBeenCalled();
+            expect(spy).toHaveBeenCalledTimes(2);
+            expect(spy).toHaveBeenCalledWith(3);
+            expect(spy).toHaveBeenCalledWith(4);
+        });
+        it('calls subtract', () => {
+            const spy = spyOn(Calculator.prototype, 'subtract');
+            calculate('3-7');
+
+            expect(Calculator.prototype.subtract).toHaveBeenCalled();
+            expect(spy).toHaveBeenCalledWith(7);
+        });
+        it('calls multiply', () => {
+            const spy = spyOn(Calculator.prototype, 'multiply');
+            calculate('3*5');
+
+            expect(Calculator.prototype.multiply).toHaveBeenCalled();
+            expect(spy).toHaveBeenCalledWith(5);
+        });
+        it('calls divide', () => {
+            const spy = spyOn(Calculator.prototype, 'divide');
+            calculate('4/2');
+
+            expect(Calculator.prototype.divide).toHaveBeenCalled();
+            expect(spy).toHaveBeenCalledWith(2);
+        });
         xit('calls updateResult');
     });
     describe('updateResult()', () => {
