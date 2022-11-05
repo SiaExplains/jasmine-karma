29efc25cf384b661fafa6e8b318949d41e3d1038
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-11-05 12:06:50 +0100

    Add coverage report for testig system :pencil:

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,29 @@
 module.exports = function(config) {
     config.set({
         frameworks: ['jasmine', 'jasmine-matchers'],
+        preprocessors: {
+            '*.js': ['coverage']
+        },
         files: [
             './custom-matchers.js',
             '*.js',
             '*.spec.js'
         ],
-        plugins: ['karma-jasmine','karma-jasmine-matchers', 'karma-chrome-launcher'],
-        reporters: ['dots'],
+        plugins: [
+            'karma-jasmine',
+            'karma-jasmine-matchers', 
+            'karma-chrome-launcher',
+            'karma-coverage'
+        ],
+        reporters: ['dots', 'coverage'],
         color: true,
         browsers: ['ChromeHeadless'],
-        singleRun: true
+        singleRun: true,
+        coverageReporter: {
+            dir: 'coverage/',
+            reporters: [
+                { type: 'html', subdir: 'html' }
+            ]
+        }
     });
 };
